8db85a5dff2531eea362c58c44374d7d346d4450
Hassel2 <hasselprassel@mail.ru>
2022-05-28 00:45:48 +0300

    Added scripts to create tabels

--- database/migrations/2022_05_27_153607_fields.php
+++ database/migrations/2022_05_27_153607_fields.php
@@ -13,11 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
+		Schema::create('fields', function(Blueprint $table) {
+			$table->id();
+			$table->foreignId('rf_subject')->constrained('rf_subjects');
+			$table->string('name', 200);
+			$table->foreignId('development_degree')->constrained('development_degree');
+		});
     }
 
     /**
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        //
     }
 };
